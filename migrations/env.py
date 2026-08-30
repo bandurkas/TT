40d5589c8e4b3e152d53d0fff9f461687c85378c
bandurkas <bandurkas@gmail.com>
2026-08-30 21:47:25 +0700

    ruff fixes

--- migrations/env.py
+++ migrations/env.py
@@ -1,9 +1,7 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
+from sqlalchemy import engine_from_config, pool
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,8 +16,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from src.db.models import Base  # noqa: E402
-from src.config.settings import settings  # noqa: E402
+from src.config.settings import settings
+from src.db.models import Base
 
 config.set_main_option("sqlalchemy.url", settings.database_url)
 target_metadata = Base.metadata
